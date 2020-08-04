af849a3010058239b9476ae0071ee47e2f7ee4d8
ORiveraJr84 <oriverajr84@gmail.com>
2020-08-03 20:38:14 -0400

    Tasks 3 & 4 complete.

--- index.js
+++ index.js
@@ -56,8 +56,13 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 
 /* Task 3: Console.log just Julius' feedback */
 
+console.log(reviews[5].feedback);
+
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
+
+reviews.push({name:"Orlando", rating: 4.5, feedback: "I thought his place was prett dope. I didn't expect to like it as much as I did! I'll DEFINITRLY be coming back here again!!"},);
+console.log(reviews[reviews.length - 1].feedback);
 
 
 /* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
